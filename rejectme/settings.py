dfae63baec96e886ba86899bb60f0be1b1f9dda7
Hack-Termux <benjaminsarpong444@gmail.com>
2025-07-24 17:40:57 +0330

    Prepare for Render deployment

--- rejectme/settings.py
+++ rejectme/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r50ncnm21cn3ie!7)b4bo+e%&7evzdvfa-tpm&tmgbjd1fv*xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
